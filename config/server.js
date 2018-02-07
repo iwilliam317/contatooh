d43513e135279711c3bbbd7153b9e63b16c5d0ad
William <willshinji@gmail.com>
2018-02-07 00:13:32 -0200

    setting middlewares

--- config/server.js
+++ config/server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 var consign = require('consign');
+var bodyParser = require('body-parser');
 
 app.set('port', 3000);
+
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(require('method-override')()) 
 
 consign()
 	.include('./app/routes')
