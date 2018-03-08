fd5af3d8242ed0d16af66fe25c42851128d79fa7
william <willshinji@gmail.com>
2018-03-08 11:11:49 -0300

    function incrementa added

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,3 +1,6 @@
 angular.module('contatooh').controller('ContatosController', function($scope) {
-	$scope.total = 0;
+  $scope.total = 0;
+  $scope.incrementa = function(){
+    $scope.total ++;
+  }
 });
